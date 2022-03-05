603215bfd63afccec6f53548093a208cac7fd575
damiano-cmd <58737783+damiano-cmd@users.noreply.github.com>
2022-03-05 11:37:29 +0100

    fix to other manifest

--- precache-manifest.959042eb45feb61168c952b09ae7ed2d.js
+++ precache-manifest.959042eb45feb61168c952b09ae7ed2d.js
@@ -1,74 +1,74 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "458f845021ee2032f83611bea7172fee",
-    "url": "/index.html"
+    "url": "/my-old-portfilio/iindex.html"
   },
   {
     "revision": "0b3faf85ac3d2407cfb4",
-    "url": "/static/css/main.b2a8fec8.chunk.css"
+    "url": "/my-old-portfilio/istatic/css/main.b2a8fec8.chunk.css"
   },
   {
     "revision": "ac0d523d2d986967cf63",
-    "url": "/static/js/2.63277276.chunk.js"
+    "url": "/my-old-portfilio/istatic/js/2.63277276.chunk.js"
   },
   {
     "revision": "9b318b6fb13190fe82c0677e9264b3c7",
-    "url": "/static/js/2.63277276.chunk.js.LICENSE.txt"
+    "url": "/my-old-portfilio/istatic/js/2.63277276.chunk.js.LICENSE.txt"
   },
   {
     "revision": "0b3faf85ac3d2407cfb4",
-    "url": "/static/js/main.9e770eb0.chunk.js"
+    "url": "/my-old-portfilio/istatic/js/main.9e770eb0.chunk.js"
   },
   {
     "revision": "4f2b1704eab49d50853b",
-    "url": "/static/js/runtime-main.19ccc7f6.js"
+    "url": "/my-old-portfilio/istatic/js/runtime-main.19ccc7f6.js"
   },
   {
     "revision": "f93094615122761cf413c32a6b22b85b",
-    "url": "/static/media/BEL.f9309461.png"
+    "url": "/my-old-portfilio/istatic/media/BEL.f9309461.png"
   },
   {
     "revision": "aa720a92d08dd7ba418b1bb238c83575",
-    "url": "/static/media/Console.aa720a92.png"
+    "url": "/my-old-portfilio/istatic/media/Console.aa720a92.png"
   },
   {
     "revision": "b8c8e249b0e72100a8280eb3bef46af2",
-    "url": "/static/media/QtBS4.b8c8e249.png"
+    "url": "/my-old-portfilio/istatic/media/QtBS4.b8c8e249.png"
   },
   {
     "revision": "e8ccd7a2f13a4d56729ece3874a346d1",
-    "url": "/static/media/SBBE.e8ccd7a2.png"
+    "url": "/my-old-portfilio/istatic/media/SBBE.e8ccd7a2.png"
   },
   {
     "revision": "1c2845b7168025a8ee372462f0501602",
-    "url": "/static/media/Svelte.1c2845b7.png"
+    "url": "/my-old-portfilio/istatic/media/Svelte.1c2845b7.png"
   },
   {
     "revision": "1011de350d50ddd0794a6297b6e46813",
-    "url": "/static/media/basicWebApp.1011de35.png"
+    "url": "/my-old-portfilio/istatic/media/basicWebApp.1011de35.png"
   },
   {
     "revision": "89c4cd1649bfca4204c33b23df6ea34f",
-    "url": "/static/media/codeScreen.89c4cd16.png"
+    "url": "/my-old-portfilio/istatic/media/codeScreen.89c4cd16.png"
   },
   {
     "revision": "24c8a074dc1d5240ad1c25b1db2467d3",
-    "url": "/static/media/lopa.24c8a074.svg"
+    "url": "/my-old-portfilio/istatic/media/lopa.24c8a074.svg"
   },
   {
     "revision": "91bcb5ebda96e9771eb49edd7a89726e",
-    "url": "/static/media/mongodb.91bcb5eb.png"
+    "url": "/my-old-portfilio/istatic/media/mongodb.91bcb5eb.png"
   },
   {
     "revision": "d0adb1bd52d94a3726e34a24c6d003dc",
-    "url": "/static/media/node-express.d0adb1bd.png"
+    "url": "/my-old-portfilio/istatic/media/node-express.d0adb1bd.png"
   },
   {
     "revision": "87c4d216cda740b290140c8bc2903487",
-    "url": "/static/media/pyLogo.87c4d216.png"
+    "url": "/my-old-portfilio/istatic/media/pyLogo.87c4d216.png"
   },
   {
     "revision": "d5bb23fcbb7e64a0fd424bd4f41fa4be",
-    "url": "/static/media/react-next.d5bb23fc.png"
+    "url": "/my-old-portfilio/istatic/media/react-next.d5bb23fc.png"
   }
 ]);
